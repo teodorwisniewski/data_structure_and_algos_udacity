b66f9e23043da938da641f00d30fd7c7d6df833a
Teodor Wisniewski <teodor.wisniewski@altmansolon.com>
2022-07-30 11:54:51 +0200

    _extract_months_days_between_dates funciton

--- 1_Introduction/how_to_solve_problems/days_between_days.py
+++ 1_Introduction/how_to_solve_problems/days_between_days.py
@@ -1,18 +1,52 @@
-from calendar import monthrange
-
+from datetime import date
 
 DAYS_OF_MONTHS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
+def _extract_months_days_between_dates(month1, month2):
+    """
+    :param month1: starting  month
+    :param month2: ending month
+    :return: 
+        examples:
+        month1=1, month2=3 -> output= [28] # February days
+        month1=1, month2=5 -> output= [28, 31, 30] # February, March and April days
+        month1=11, month2=2 -> output= [31, 31] # December and January days
+        month1=11, month2=12 -> output= [0] # 0 days
+        month1=12, month2=1 -> output= [0] # 0 days
+    """
+    if abs(month1 - month2) < 2:
+        return [0]
+    elif month2 > month1:
+        extracted_days = DAYS_OF_MONTHS[month1:month2-1]
+        return extracted_days
+    else:
+        till_end_of_year = DAYS_OF_MONTHS[month1:12] if month1 < 12 else []
+        from_the_start_of_the_year = DAYS_OF_MONTHS[0:(month2-1)] if month1 > 1 else []
+        extracted_days = till_end_of_year + from_the_start_of_the_year
+        return extracted_days
+
 
 def daysBetweenDates(year1, month1, day1, year2, month2, day2):
     """
     Calculates the number of days between two dates.
     """
+    if (year1, month1, day1) == (year2, month2, day2):
+        return 0
 
-    # TODO - by the end of this lesson you will have
-    #  completed this function. You do not need to complete
-    #  it yet though!
+    delta_years = abs(year2 - year1)
 
-    return 0
+    nb_days = DAYS_OF_MONTHS[month1-1] - day1
+    nb_days += day2
+
+    if month1 != month2 and abs(month1 - month2) > 1:
+         starting_mon, ending_month = (month1, month2) if month1 < month2 else (12-month1, )
+         days_of_months_to_add = DAYS_OF_MONTHS[starting_mon:ending_month-2]
+         nb_days += sum(days_of_months_to_add)
+    elif month1 == month2 and year1 == year2:
+        return day2 - day1
+
+    return nb_days
 
 
 def testDaysBetweenDates():
@@ -22,6 +56,15 @@ def testDaysBetweenDates():
     # test adjacent days
     assert (daysBetweenDates(2017, 12, 30,
                              2017, 12, 31) == 1)
+
+    # test adjacent days
+    assert (daysBetweenDates(2017, 12, 31,
+                             2018, 2, 1) == (date(2018, 2, 1) - date(2017, 12, 31)).days)
+
+    assert (daysBetweenDates(2017, 12, 31,
+                             2019, 2, 1) == (date(2019, 2, 1) - date(2017, 12, 31)).days)
+
+
     # test new year
     assert (daysBetweenDates(2017, 12, 30,
                              2018, 1, 1) == 2)
@@ -33,10 +76,7 @@ def testDaysBetweenDates():
     print("function is working correctly!")
 
 
-testDaysBetweenDates()
-
-
 if __name__ == "__main__":
-    test()
+    testDaysBetweenDates()
 
     print("end")
