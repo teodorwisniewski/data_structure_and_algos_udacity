4f10ec3afcde623803541452a8c8bf38df1eba49
Teodor Wisniewski <teodor.wisniewski@altmansolon.com>
2022-08-29 18:12:31 +0200

    Balanced Parentheses Exercise - Stacks

---
+++ 2_data_structure/stack/Balanced Parentheses Exercise - Stacks.py
@@ -0,0 +1,42 @@
+# Our Stack Class - Brought from previous concept
+# No need to modify this
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def size(self):
+        return len(self.items)
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if self.size() == 0:
+            return None
+        else:
+            return self.items.pop()
+
+
+def equation_checker(equation):
+    """
+    Check equation for balanced parentheses
+
+    Args:
+       equation(string): String form of equation
+    Returns:
+       bool: Return if parentheses are balanced or not
+    """
+    stack = Stack()
+    open_bracket_count = 0
+    closed_bracket_count = 0
+    for char in equation:
+        if char == "(":
+            stack.push(char)
+        elif char == ")":
+            if stack.pop() == None:
+                return False
+
+    return stack.size() == 0
+
+print ("Pass" if (equation_checker('((3^2 + 8)*(5/2))/(2+6)')) else "Fail")
+print ("Pass" if not (equation_checker('((3^2 + 8)*(5/2))/(2+6))')) else "Fail")
